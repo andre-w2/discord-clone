073a986f49f70724ab9d6c65e676a69f43731b6d
andre-w2 <cibisova267@gmail.com>
2023-10-03 18:38:20 +0500

    Закончил загрузку файла

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,23 +1,22 @@
+import { auth } from '@clerk/nextjs'
 import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
+
 const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); 
- 
+
+const handleAuth = () => {
+    const { userId } = auth()
+    if (!userId) throw new Error("Unauthorized")
+
+    return { userId: userId }
+}
+
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
-      const user = await auth(req);
- 
-      if (!user) throw new Error("Unauthorized");
- 
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
-    }),
+    serverImage: f({ image: { maxFileSize: '4MB', maxFileCount: 1 } })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { }),
+    messageFile: f(["image", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => { })
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
