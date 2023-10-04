d9365356580e6f3f3b3b33054613257ece47e007
andre-w2 <cibisova267@gmail.com>
2023-10-04 14:21:03 +0500

    edit server && fix in invite-code

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
-
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { NextResponse } from "next/server"
@@ -7,13 +5,10 @@ import { NextResponse } from "next/server"
 export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
     try {
         const profile = await currentProfile()
+        const { name, imageUrl } = await req.json()
 
         if (!profile) {
             return new NextResponse("Unauthorized", { status: 401 })
-        }
-
-        if (!params.serverId) {
-            return new NextResponse("Server ID Missing", { status: 400 })
         }
 
         const server = await db.server.update({
@@ -22,13 +17,14 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
                 profileId: profile.id
             },
             data: {
-                inviteCode: uuidv4()
+                name,
+                imageUrl
             }
         })
 
         return NextResponse.json(server)
     } catch (error) {
-        console.log("[SERVED_ID]", error)
+        console.log("[SERVED_ID_PATCH]", error)
         return new NextResponse("Internal Error", { status: 500 })
     }
 }
