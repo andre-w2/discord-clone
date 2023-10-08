fbfb3e72fde7446fb15ca57dd66bd1fd4e140ffe
andre-w2 <cibisova267@gmail.com>
2023-10-08 17:07:16 +0500

    чат шапка

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -13,7 +13,7 @@ const ServerIdPage = async ({ params }: ServerIdPageProps) => {
   const profile = await currentProfile();
 
   if (!profile) {
-    return redirectToSignIn();
+    return redirect("/")
   }
 
   const server = await db.server.findUnique({
